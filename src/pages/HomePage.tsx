5dff627e9cf1745976166bac2f7f176a8ac5ac7f
StefanTodoran <stefan.alex4@gmail.com>
2023-10-15 09:29:03 -0700

    untracked files

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,20 @@
+import PageHeading from "../components/PageHeading";
+
+import HomeIcon from "../assets/logo-icon.svg";
+import "../styles/HelpPage.css";
+
+export default function HomePage() {
+
+  return (
+    <>
+      <PageHeading iconSrc={HomeIcon}>Home</PageHeading>
+
+      <p className="hint">
+        Welcome to Mixtify, a modern web app designed to be a bridge between
+        Spotify, Apple Music, Amazon Music, and other streaming services, 
+        allowing friends to come together and share in the joy of music no 
+        matter the services they use.
+      </p>
+    </>
+  );
+}
